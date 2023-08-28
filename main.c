7aef563c980e7ea2b10d83d7d5d9c419be3e506d
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-27 17:48:55 -0700

    fix compilation issues

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int data_structure = 0;
 int main(int argc, char *argv[])
 {
 	FILE *file;
+	char *opcode;
 	char buffer[256];
 	unsigned int line_number = 1;
-	char *opcode;
 	stack_t *stack = NULL;
 
 	if (argv[1] == NULL || argc != 2)
@@ -30,15 +30,10 @@ int main(int argc, char *argv[])
 	while (fgets(buffer, sizeof(buffer), file) != NULL)
 	{
 		void (*op_fn)(stack_t **, unsigned int);
-		char *op_arg;
 
 		if (buffer[0] == '#')
 			continue;
 		opcode = strtok(buffer, " \n");
-		if (strcmp(opcode, "push") == 0)
-		{
-			op_arg = strtok(NULL, " ");
-		}
 		op_fn = op_check(opcode, line_number);
 		op_fn(&stack, line_number);
 		line_number++;
